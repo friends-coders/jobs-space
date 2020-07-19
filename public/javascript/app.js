203eed72b6529027d2af66328388c7bc958f680c
AhmadKheder <ahmadkhederabdulatef@gmail.com>
2020-07-19 22:13:05 +0300

    for signup

---
+++ public/javascript/app.js
@@ -0,0 +1,11 @@
+//   signI/Up Start
+  //Tabs Layout Code
+  $("#tabs").tabs({
+    activate: function (event, ui) {
+      var active = $("#tabs").tabs("option", "active");
+      $("#tabid").html(
+        "the tab id is " + $("#tabs ul>li a").eq(active).attr("href")
+      );
+    },
+  });
+  //   signI/Up End
