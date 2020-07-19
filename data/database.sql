4f4a27e4a5fb4978481edb5c6e9465277b8ce0d6
AhmadKheder <ahmadkhederabdulatef@gmail.com>
2020-07-19 22:15:02 +0300

    users and hiring tables

--- data/database.sql
+++ data/database.sql
@@ -1,16 +1,34 @@
-DROP TABLE IF EXISTS clients;
+
+DROP TABLE IF EXISTS users;
 CREATE TABLE
-IF NOT EXISTS clients
+IF NOT EXISTS users
 (
-    id SERIAL PRIMARY KEY,
-    pic VARCHAR(255),
-    fullname VARCHAR(255),
-    specialistIn VARCHAR(255),
-    bio VARCHAR(255),
+    user_id SERIAL,
+    img VARCHAR(255),
+    fullName VARCHAR(255),
+    password VARCHAR(255),
+    gender VARCHAR(255),
+    education VARCHAR(255),
+    major VARCHAR(255),
+    email VARCHAR(255),
     linkedIn VARCHAR(255),
     github VARCHAR(255),
-    instagram VARCHAR(255)
+    bio VARCHAR(255),
+     PRIMARY KEY(user_id,email)
 );
 
+INSERT INTO users (img,fullName,password ,gender,education,major,email,linkedIn,github,bio) VALUES('myImage-1595103536839','Ali','ali','male','phd','ML','ali@gmail.com','Linkedin/ali','github/ali','I am Aloosh');
 
-INSERT INTO clients (pic,fullname,specialistIn,bio,linkedIn,github,instagram) VALUES('Aman.png','ahmad xd','web','bjkkasawd','https://www.linkedin.com/in/ahmad-abdulatef-121066181',null,null);
+
+DROP TABLE IF EXISTS hiring;
+CREATE TABLE
+IF NOT EXISTS hiring
+(
+    user_id SERIAL PRIMARY KEY,
+    img VARCHAR(255),
+    fullName VARCHAR(255),
+    major VARCHAR(255),
+    linkedIn VARCHAR(255),
+    github VARCHAR(255),
+    description VARCHAR(255)
+);
