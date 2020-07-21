e3ef4253ccfe51183d6b4f600d22d955d3f55088
abdulelahxd <abdulelahkassara@gmail.com>
2020-07-21 06:03:42 +0300

    add event

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -14,29 +14,14 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-//   signI/Up Start
-  //Tabs Layout Code
-  // $("#tabs").tabs({
-  //   activate: function (event, ui) {
-  //     var active = $("#tabs").tabs("option", "active");
-  //     $("#tabid").html(
-  //       "the tab id is " + $("#tabs ul>li a").eq(active).attr("href")
-  //     );
-  //   },
-  // });
-  //   signI/Up End
+// /* Demo purposes only */
+// for hire me page
+$(".hover").mouseleave(
+  function() {
+    $(this).removeClass("hover");
+  }
+);
 
-// console.log('hiiiiiiiiiiiiiiiiiiiiiii thehtererer')
-
-// $('#ahhaha').on("submit", function(event){
-    // event.preventDefault();
-    // location.reload();
-    // console.log($('input[type="radio"]:checked').val())
-// })
-
-// $('input[type="radio"]').on('change', function() {
-//     $('input[type="radio"]').not(this).prop('checked', false);
-//  });
 let user;
 let pass;
 
